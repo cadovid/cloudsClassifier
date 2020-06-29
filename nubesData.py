097d14af425a0a0bccefc2a16c1b56e41eb4fdfe
David Camino <david.camino.perdones@gmail.com>
2020-06-29 20:38:18 +0200

    Update nubesData.py

--- nubesData.py
+++ nubesData.py
@@ -1,35 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as pl
 
-semeion_x = np.load("nubes_train.npy")
-semeion_y = np.load("labels_train.npy")
-# semeion_validation_x = np.load("nubes_validation.npy")
-# semeion_validation_y = np.load("labels_validation.npy")
-# semeion_test_x = np.load("nubes_test.npy")
-# semeion_test_y = np.load("labels_test.npy")
-
-# print(semeion_x.shape)
-# print(semeion_y.shape)
-# print(semeion_x)
-# print(semeion_y)
-# print(semeion_validation_x.shape)
-# print(semeion_validation_y.shape)
-# print(semeion_validation_y)
-# print(semeion_test_x.shape)
-# print(semeion_test_y.shape)
-# print(semeion_test_y)
+clouds_x = np.load("nubes_train.npy")
+clouds_y = np.load("labels_train.npy")
 
 # Busqueda de los elementos que nos interesa segun su clase
-elementPosition = np.where(semeion_y == 4)
+elementPosition = np.where(clouds_y == 4)
 print(elementPosition)
 for i in elementPosition:
-    aa = semeion_y[i]
+    aa = clouds_y[i]
 
 np.save('ValuesClass', aa)
 
 # Guardamos en un array todos los datos del array original que nos interesa
 for i in elementPosition:
-    qq = semeion_x[i]
+    qq = clouds_x[i]
     print(qq.shape)
 
 np.save('searchedClass', qq)
